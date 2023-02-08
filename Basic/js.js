84364b3b12508e9adf6a776c9486c98c09a51b8f
Nenad Vasiljev <vasiljevnenad1@gmail.com>
2023-02-08 11:24:18 +0100

    init

---
+++ Basic/js.js
@@ -0,0 +1,86 @@
+let toyotaAuris = {
+    brand: 'Toyota',
+    age: 2007,
+    HP: 125,
+    fuel: 'Diesel',
+
+    print: function() {
+        return `Toyota Auris is ${this.brand} brand and is ${this.age}, has ${this.HP} and fuel type is ${this.fuel}`
+    }
+}
+
+let bmw320 = {
+    brand: 'BMW',
+    age: 2010,
+    HP: 200,
+    fuel: 'Gasoline',
+
+    print: function() {
+        return `BMW 320i is ${this.brand} brand and is ${this.age}, has ${this.HP} and fuel type is ${this.fuel}`
+    }
+}
+
+console.log(bmw320.print())
+
+let hondaCivic = {
+    brand: 'Honda',
+    age: 1997,
+    HP: 150,
+    fuel: 'Gasoline',
+
+    print: function() {
+        return `Honda Civic is ${this.brand} brand and is ${this.age}, has ${this.HP} and fuel type is ${this.fuel}`
+    }
+}
+
+let cat = {
+    type: 'Animal',
+    name: 'Jerry',
+    age: 3,
+
+    print: function() {
+        return `Cat is the type of ${this.type} and it's name is ${this.name} is ${this.age} old`
+    }
+}
+
+let aligator = {
+    type: 'Animal',
+    name: 'Dundy',
+    age: 10,
+
+    print: function() {
+        return `Cat is the type of ${this.type} and it's name is ${this.name} is ${this.age} old`
+    }
+}
+
+let dog = {
+    type: 'Animal',
+    name: 'Jacky',
+    age: 6,
+
+    print: function() {
+        return `Cat is the type of ${this.type} and it's name is ${this.name} is ${this.age} old`
+    }
+}
+
+let Tom = {
+    job: 'IT',
+    age: 36,
+    location: 'Vienna',
+
+    print: function() {
+        return 
+    }
+}
+
+let Jimmy = {
+    job: 'Construction',
+    age: 48,
+    location: 'Bratislava'
+}
+
+let Robert = {
+    job: 'Taxi Driver',
+    age: 44,
+    location: 'New York'
+}
